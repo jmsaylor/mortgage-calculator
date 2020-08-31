9aee9b30571046610003f6ef1acb81c91d503d32
John <johnmsaylor@gmail.com>
2020-08-31 07:54:45 -0400

    added new method

--- Main.java
+++ Main.java
@@ -13,8 +13,13 @@ public class Main {
         double interest = readNumber("Enter interest rate: ", 0, 50);
 
         float monthlyPayment = (float)calculateMortgage(principal, interest, term);
-
         System.out.println(monthlyPayment);
+
+        for (int i = 0; i < term * 12; i++ ) {
+            float balance = (float)calculateBalance(principal, i,interest,term);
+            System.out.println(balance);
+        }
+
 
     }
 
@@ -31,13 +36,32 @@ public class Main {
 
     }
 
+    public static double calculateBalance(int principal, int month, double interest, int term) {
+
+        interest = (interest / 100) / 12;
+        term = term * 12;
+
+        double answer = Math.pow((1 + interest), term) - Math.pow((1 + interest), month);
+        answer = principal * answer;
+        answer = answer / (Math.pow((1 + interest), term) - 1);
+
+        return Math.ceil(answer);
+
+    }
+
+
+
+
+
     public static double calculateMortgage(int principal, double interest, int term) {
         interest = (interest / 100) / 12;
         term = term * 12;
+
         double answer = (interest * Math.pow(interest + 1, term));
         answer = answer / (Math.pow(interest + 1, term) - 1);
 
         return Math.ceil(principal * answer);
 
     }
+
 }
