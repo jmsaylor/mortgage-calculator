ce8d89cfa704c17276b1b365ef20bb6b49d36236
John <johnmsaylor@gmail.com>
2020-09-06 15:26:30 -0400

    refactored toward OOP

--- Main.java
+++ Main.java
@@ -1,67 +1,21 @@
 package com.johnmsaylor;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Loan - Monthly Payment Calculator");
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
-        int principal = (int)readNumber("Enter principal amount: ", 100000, 1000000);
-        int term = (int)readNumber("Enter term of loan (years): ", 10, 30);
-        double interest = readNumber("Enter interest rate: ", 0, 50);
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
 
-        float monthlyPayment = (float)calculateMortgage(principal, interest, term);
-        System.out.println(monthlyPayment);
+        var report = new MortgageReport(calculator);
 
-        for (int i = 0; i < term * 12; i++ ) {
-            float balance = (float)calculateBalance(principal, i,interest,term);
-            System.out.println(balance);
-        }
-
-
-    }
-
-    public static double readNumber(String prompt, int min, int max) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(prompt);
-        double value = 0;
-        while (true) {
-            value = scanner.nextDouble();
-            if (value >= min && value <= max) break;
-            System.out.println("Enter a value between " + min + "and " + max);
-        }
-        return value;
-
-    }
-
-    public static double calculateBalance(int principal, int month, double interest, int term) {
-
-        interest = (interest / 100) / 12;
-        term = term * 12;
-
-        double answer = Math.pow((1 + interest), term) - Math.pow((1 + interest), month);
-        answer = principal * answer;
-        answer = answer / (Math.pow((1 + interest), term) - 1);
-
-        return Math.ceil(answer);
-
-    }
-
-
-
-
-
-    public static double calculateMortgage(int principal, double interest, int term) {
-        interest = (interest / 100) / 12;
-        term = term * 12;
-
-        double answer = (interest * Math.pow(interest + 1, term));
-        answer = answer / (Math.pow(interest + 1, term) - 1);
-
-        return Math.ceil(principal * answer);
-
+        report.printMortgage();
+        report.printPaymentSchedule();
     }
 
 }
+
---
+++ MortgageReport.java
@@ -0,0 +1,34 @@
+package com.johnmsaylor;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+
+    private final NumberFormat currency;
+    private MortgageCalculator calculator;
+
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
+    }
+
+    public void printMortgage() {
+        double monthlyPayment = calculator.calculateMortgage();
+        String monthlyPaymentFormatted = currency.format(monthlyPayment);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + monthlyPaymentFormatted);
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+
+        for (double balance : calculator.getRemainingBalances()) {
+            System.out.println(currency.format(balance));
+        }
+
+    }
+}
